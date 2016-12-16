925bc5b8299c9214200cf2495adae0bda660189b
Jason Yang (AZURE STORAGE) <yabo@microsoft.com>
2016-12-16 22:57:01 +0800

    update

--- Tuning/Tuning.cpp
+++ Tuning/Tuning.cpp
@@ -7,6 +7,9 @@
 
 #include "was\storage_account.h"
 #include "was\blob.h"
+
+#include "cpprest\rawptrstream.h"
+
 const int times = 5;
 const int parallel = 20;
 
@@ -22,7 +25,7 @@ public:
     // return the time costs in milli-seconds.
 	int single_download_test(size_t length, int parallelism);
 
-	int single_download_test_memory_allocated(size_t length, int parallelism);
+	int single_download_test_raw_memory(size_t length, int parallelism);
     
     int single_download_test_filestream(size_t length, int parallelism);
 
@@ -40,7 +43,7 @@ public:
 		}
 		return out.str();
 	}
-	utility::string_t download_test_memory_allocated()
+	utility::string_t download_test_raw_memory()
 	{
 		utility::stringstream_t out;
 		std::vector<size_t> lens{ 3, 30, 100, 1000 };
@@ -49,7 +52,7 @@ public:
 			size_t length(lens[index]);
 			for (int i = 1; i <= parallel; ++i)
 			{
-				ucout << length << L" " << i << " " << single_download_test_memory_allocated(length, i) << std::endl;
+				ucout << length << L" " << i << " " << single_download_test_raw_memory(length, i) << std::endl;
 			}
 		}
 		return out.str();
@@ -122,7 +125,7 @@ int Performance::single_download_test(size_t length, int parallelism)
     return total / times;
 }
 
-int Performance::single_download_test_memory_allocated(size_t length, int parallelism)
+int Performance::single_download_test_raw_memory(size_t length, int parallelism)
 {
 	azure::storage::cloud_blob_container container = client.get_container_reference(L"perftests");
 	container.create_if_not_exists();
@@ -153,19 +156,20 @@ int Performance::single_download_test_memory_allocated(size_t length, int parall
 			}
 		}
 	}
-	int total = 0;
-	for (int i = 0; i < times; ++i)
-	{
-		// download data
-		option.set_disable_content_md5_validation(true);
-		option.set_parallelism_factor(parallelism);
-		concurrency::streams::container_buffer<std::vector<uint8_t>> download_buffer;
-		download_buffer.alloc(length * 1024 * 1024);
-		auto start = clock();
-		blob.download_to_stream(download_buffer.create_ostream(), azure::storage::access_condition(), option, azure::storage::operation_context());
-		auto end = clock();
-		total += end - start;
-	}
+    int total = 0;
+    for (int i = 0; i < times; ++i)
+    {
+        // download data
+        option.set_disable_content_md5_validation(true);
+        option.set_parallelism_factor(parallelism);
+        uint8_t* buffer = new uint8_t[length * 1024 * 1024];
+        auto download_buffer = concurrency::streams::rawptr_stream<uint8_t>::open_ostream(buffer, length * 1024 * 1024);
+        auto start = clock();
+        blob.download_to_stream(download_buffer, azure::storage::access_condition(), option, azure::storage::operation_context());
+        auto end = clock();
+        total += end - start;
+        delete[] buffer;
+    }
 
 	return total / times;
 }
@@ -174,7 +178,7 @@ int Performance::single_download_test_filestream(size_t length, int parallelism)
 {
     azure::storage::cloud_blob_container container = client.get_container_reference(L"perftests");
     container.create_if_not_exists();
-    azure::storage::cloud_append_blob blob = container.get_append_blob_reference(L"target");
+    azure::storage::cloud_append_blob blob = container.get_append_blob_reference(utility::conversions::print_string(length));
 	azure::storage::blob_request_options option;
 
 	if (!(blob.exists() && (blob.properties().size() == length * 1024 * 1024)))
@@ -221,13 +225,14 @@ int Performance::single_download_test_filestream(size_t length, int parallelism)
 int main()
 {
     Performance instance(L"DefaultEndpointsProtocol=https;AccountName=jasonunittest;AccountKey=BD1ax46VII51dNjVzwuBiMMbwh7PIwPxu010yHtd+r34Pzmd1y8PsVXNKT1Hz0j6J8SulwPiHEIVuemo+eyN1A==");
-	// ucout << instance.single_download_test_memory_allocated(1000, 5) << std::endl;
-	// ucout << instance.single_download_test_filestream(1000, 5) <<std::endl;
+	//ucout << instance.single_download_test(1000, 8) << std::endl;
+	//ucout << instance.single_download_test_raw_memory(1000, 8) << std::endl;
+	// ucout << instance.single_download_test_filestream(1000, 8) <<std::endl;
 	ucout << instance.download_test() << std::endl;
 	ucout << L"*******************************************" << std::endl;
-	ucout << instance.download_test_memory_allocated() << std::endl;
-    ucout << L"*******************************************" << std::endl;
-    ucout << instance.download_test_filestream() << std::endl;
+	ucout << instance.download_test_raw_memory() << std::endl;
+	ucout << L"*******************************************" << std::endl;
+	ucout << instance.download_test_filestream() << std::endl;
 
     return 0;
 }
